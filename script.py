update: re-implemented evaluation mode, fixed iou calculation

script.py
@@ -13,8 +13,7 @@ from src.briscloader import LoadBRISC
 from src.models.cam import CAM
 from src.models.gradcam import GradCAM
 from src.trainloop.forward_trainer import ForwardTrainer
-from src.trainloop.backward_trainer import BackwardTrainer
-# from src.evaluate import evaluate,plot_confmat,plot_history,plot_sample
+from src.evaluate import evaluate,plot_confmat,plot_history,plot_sample
 
 import mlflow
 from src.config import CONFIG
@@ -40,7 +39,9 @@ mlflow.set_experiment('CAM (Zhang et al, 2018)')
 
 # READ DATA ================================================================================================================
 loader = LoadBRISC()
-train_ds,val_ds,test_ds = loader.load(classes='tumor',
+### !!! IMPORTANT !!!
+### Tumor only selection has not been implemented in the evaluation module
+train_ds,val_ds,test_ds = loader.load(classes='tumor', # type: ignore
                                       planes='all',
                                       encoding_type='onehot',
                                       split_val=True,
@@ -61,8 +62,7 @@ test_dl = DataLoader(test_ds,
                      drop_last=False)
 
 # MODEL TRAINING
-MODEL= GradCAM(len(loader.classes),backbone_model_type=CONFIG['backbone_model_type'],ch_project=CONFIG['ch_project'])
-EPOCHS = 5
+MODEL= CAM(len(loader.classes),backbone_model_type=CONFIG['backbone_model_type'],ch_project=CONFIG['ch_project'])
 LR = 1e-3
 WD = 1e-4
 CRITERION = nn.CrossEntropyLoss()
@@ -70,27 +70,26 @@ OPTIMIZER = torch.optim.AdamW(MODEL.parameters(),lr=LR,weight_decay=WD)
 SCHEDULER = torch.optim.lr_scheduler.ReduceLROnPlateau(OPTIMIZER, mode='min', factor=0.1, patience=10)
 
 loop = ForwardTrainer(MODEL,CRITERION,OPTIMIZER,device,SCHEDULER)
-history = loop.fit(train_dl,val_dl,EPOCHS)
+history = loop.fit(train_dl,val_dl,CONFIG['epochs'])
 torch.save(MODEL.state_dict(),f'saves/CAM_{CONFIG['run_name']}.pth')
 print(f'Training completed in {loop.fit_time:.4f} seconds.')
 
-# # MODEL EVALUATION
-# results = evaluate(MODEL,test_dl)
-# (acc,dsc,iou) = results['metrics']
-# (Xs,ys,y_preds,Ms,cams) = results['data']
-# M_bbox,cam_bbox = results['bbox']
-# fit_time = loop.fit_time
-# print(f'----- TEST SET MODEL EVALUATION -----')
-# print(f'Accuracy: {acc:.4f}')
-# print(f'IoU     : {iou:.4f}')
-# print(f'DSC     : {dsc:.4f}')
-# print(f'Time    : {fit_time:.4f} s')
+# MODEL EVALUATION
+results = evaluate(MODEL,test_dl)
+(acc,dsc,iou) = results['metrics']
+data = results['data']
+fit_time = loop.fit_time
+print(f'----- TEST SET MODEL EVALUATION -----')
+print(f'Accuracy: {acc:.4f}')
+print(f'IoU     : {iou:.4f}')
+print(f'DSC     : {dsc:.4f}')
+print(f'Time    : {fit_time:.4f} s')
 
-# hist_fig = plt.figure(figsize=(15,4))
-# plot_history(EPOCHS,history)
+hist_fig = plt.figure(figsize=(15,4))
+plot_history(CONFIG['epochs'],history)
 
-# confmat_fig = plt.figure(figsize=(7,6))
-# plot_confmat(ys,y_preds,classes=loader.classes)
+confmat_fig = plt.figure(figsize=(7,6))
+plot_confmat(data['y'],data['y_pred'],classes=loader.classes)
 
 # # LOGGING
 # with mlflow.start_run(run_name=CONFIG['run_name']):
@@ -101,7 +100,7 @@ print(f'Training completed in {loop.fit_time:.4f} seconds.')
 #         'batchsize':CONFIG['batch_size'],
 #         'ch_project':CONFIG['ch_project'],
 #         'threshold':CONFIG['threshold'],
-#         'epochs':EPOCHS,
+#         'epochs':CONFIG['epochs'],
 #         'lr':LR,
 #         'wd':WD
 #     })

src/briscloader.py
@@ -155,7 +155,7 @@ class LoadBRISC():
             cls = self.classes_abr[id.split('_')[1]]
             # img_path = glob.glob(os.path.join(self.cls_root,f'{mode}/{cls}/*{id}*'))[0]
             img_path = os.path.join(self.cls_root,f'{mode}/{cls}/brisc2025_{mode}_{id}.jpg')
-            img = to_image(Image.open(img_path))
+            img = to_image(Image.open(img_path).convert("L"))
             img = v2.functional.resize(img,(224,224))
             img = v2.Grayscale(num_output_channels=1)(img)
             if cls == 'no_tumor':
@@ -163,13 +163,13 @@ class LoadBRISC():
             else:
                 # mask_path = glob.glob(os.path.join(self.seg_root,f'{mode}/masks/*{id}*'))[0]
                 mask_path = os.path.join(self.seg_root,f'{mode}/masks/brisc2025_{mode}_{id}.png')
-                mask = to_image(Image.open(mask_path))
+                mask = to_image(Image.open(mask_path).convert("L"))
                 mask = v2.functional.resize(mask,(224,224))
                 mask = (mask >= 0.7)
             imgs.append(img)
             masks.append(mask)
-        imgs = torch.cat(imgs).to(torch.float32).unsqueeze(1)
-        masks = torch.cat(masks).to(torch.float32).unsqueeze(1)
+        imgs = torch.stack(imgs).to(torch.float32)/255
+        masks = torch.stack(masks).to(torch.float32)
         return imgs,masks
 
 class DatasetClass(Dataset):

src/evaluate.py
@@ -2,69 +2,132 @@ import numpy as np
 import torch
 from torch import nn
 from torchmetrics.functional.segmentation import dice_score
-from torchmetrics.detection.iou import IntersectionOverUnion
+from torchmetrics.functional.detection.iou import intersection_over_union
 from torchvision.ops import masks_to_boxes
 from sklearn.metrics import accuracy_score,confusion_matrix
 import matplotlib.pyplot as plt
+from matplotlib import patches
 import seaborn as sns
-from trainloop import val_step # NOT YET IMPLEMENTED
 
-def evaluate(model,test_dataloader,criterion=nn.CrossEntropyLoss()):
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    _, preds = val_step(model,criterion,test_dataloader,device=device,return_data=True)
-    Xs,ys,y_preds,Ms,cams = preds
+from src.config import CONFIG
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+def make_preds(model,dataloader) -> dict:
+    model.to(device)
+    model.eval()
+    data = {
+        'X':[],
+        'M':[],
+        'y':[],
+        'y_pred':[],
+        'cam':[],
+        'mask':[]
+    }
+    with torch.no_grad():
+        for batch,(X,M,y) in enumerate(dataloader):
+            X,M,y = X.to(device),M.to(device),y.argmax(1).to(device).to(torch.float32)
+            y_logits = model(X)
+            y_preds = torch.softmax(y_logits,1).argmax(1)
+            y_cams = model.get_cam(threshold='raw')
+            y_masks = model.get_cam(threshold=CONFIG['threshold'])
+
+            data['X'].extend(X)
+            data['M'].extend(M)
+            data['y'].extend(y)
+            data['y_pred'].extend(y_preds)
+            data['cam'].extend(y_cams)
+            data['mask'].extend(y_masks)
+        data['X'] = torch.cat(data['X'],dim=0).cpu() # type: ignore
+        data['M'] = torch.cat(data['M'],dim=0).cpu().to(torch.long)  # type: ignore
+        data['y'] = torch.stack(data['y']).cpu()  # type: ignore
+        data['y_pred'] = torch.stack(data['y_pred']).cpu()  # type: ignore
+        data['cam'] = torch.cat(data['cam'],dim=0).cpu()  # type: ignore
+        data['mask'] = torch.cat(data['mask'],dim=0).cpu().to(torch.long)  # type: ignore
+    return data 
+
+def evaluate(model,dataloader):
+    data = make_preds(model,dataloader)
     # Accuracy
-    acc = accuracy_score(ys,y_preds)
+    acc = accuracy_score(data['y'],data['y_pred'])
     # Dice Similarity Coef.
-    dsc = dice_score(cams,Ms,num_classes=2).median()
-    # IoU (n = number of target PER IMAGE)
-    cam_bbox = [{
-        'boxes': masks_to_boxes(cams[i].unsqueeze(0)), # nhw --> xyxy
-        'labels': y_preds[i].unsqueeze(0) # n
-    } for i in range(Xs.shape[0])]
-    M_bbox = [{
-        'boxes': masks_to_boxes(Ms[i].unsqueeze(0)), # nhw --> xyxy
-        'labels': ys[i].unsqueeze(0) # n 
-    } for i in range(Xs.shape[0])]
-    iou = IntersectionOverUnion()(cam_bbox,M_bbox)['iou']
+    dsc = dice_score(data['mask'],data['M'],num_classes=2,include_background=False,average='macro',input_format='index').median()
+    # IoU
+    M_bbox = masks_to_boxes(data['M'])
+    mask_bbox = masks_to_boxes(data['mask'])
+    iou = intersection_over_union(mask_bbox,M_bbox,aggregate=True)
+
+    data['M_bbox'] = M_bbox
+    data['mask_bbox'] = mask_bbox
 
     results = {
         'metrics':(acc,dsc,iou),
-        'data':(Xs,ys,y_preds,Ms,cams),
-        'bbox':(M_bbox,cam_bbox)
+        'data':data
     }
     return results
 
 def plot_confmat(ys,y_preds,classes=None):
     confmat = confusion_matrix(ys,y_preds)
     classes = classes if classes else [i for i in range(len(np.unique(ys)))]
-    sns.heatmap(confmat,annot=True,fmt='d',cmap='viridis',xticklabels=classes,yticklabels=classes)
+    sns.heatmap(confmat,annot=True,fmt='d',cmap='viridis',xticklabels=classes,yticklabels=classes)  # type: ignore
     plt.xlabel('Predicted Label')
     plt.ylabel('True Label')
 
-def plot_sample(Xs,ys,y_preds,Ms,cams,index=None):
+def plot_sample(data,index=None,classes=None):
+    '''
+    Plots sample image, its ground truth (GT) mask, predicted mask, and raw CAM.
+    Args:
+        data (dict): Data dictionary obtained from evaluate() function.
+        index (int, default=None): Index of the selected sample. If none, randomly pick a sample form the provided data.
+        classes (list, default=None): List of class labels. If none, numeric-encoded class labels will be used.
+    Returns:
+        None
+    '''
     if index is None:
-        index = np.random.randint(Xs.shape[0])
-    
-    plt.figure(figsize=(10,4))
-    plt.subplot(1,3,1)
-    plt.imshow(Xs[index].squeeze().cpu())
-    plt.axis(False)
-    plt.title(f'True: {ys[index]} | Pred: {y_preds[index]}')
+        index = np.random.randint(data['X'].shape[0])
 
-    M = (Ms[index].squeeze().cpu() >= 0.5).long()
-    plt.subplot(1,3,2)
+    X = data['X'][index]
+    M = data['M'][index]
+    y = data['y'][index].to(int)
+    y_pred = data['y_pred'][index].to(int)
+    cam = data['cam'][index]
+    mask = data['mask'][index]
+    M_bbox = data['M_bbox'][index]
+    mask_bbox = data['mask_bbox'][index]
+
+    M_patch = patches.Rectangle((M_bbox[0],M_bbox[1]),M_bbox[2]-M_bbox[0],M_bbox[3]-M_bbox[1],fill=False,ec='lime',fc=None)
+    mask_patch = patches.Rectangle((mask_bbox[0],mask_bbox[1]),mask_bbox[2]-mask_bbox[0],mask_bbox[3]-mask_bbox[1],fill=False,ec='magenta',fc=None)
+
+    plt.subplot(1,4,1)
+    ax = plt.gca()
+    ax.imshow(X)
+    ax.add_patch(M_patch)
+    ax.add_patch(mask_patch)
+    plt.text(M_bbox[0],M_bbox[1],s='GT',color='lime')
+    plt.text(mask_bbox[0],mask_bbox[1],s='Pred',color='magenta')
+    plt.axis(False)
+    plt.title('Image')
+
+    plt.subplot(1,4,2)
     plt.imshow(M)
     plt.axis(False)
     plt.title('GT Mask')
 
-    cam = (cams[index].squeeze().cpu() >= 0.5).long()
-    plt.subplot(1,3,3)
+    plt.subplot(1,4,3)
+    plt.imshow(mask)
+    plt.axis(False)
+    plt.title('Predicted Mask')
+
+    plt.subplot(1,4,4)
     plt.imshow(cam)
     plt.axis(False)
-    plt.title('CAM')
+    plt.title('Upscaled CAM')
 
-    plt.suptitle(f'DSC = {dice_score(cam.unsqueeze(0),M.unsqueeze(0),num_classes=2,include_background=False,average='macro',input_format='index').item():.2f}',y=0.9)
+    dsc = dice_score(mask.unsqueeze(0),M.unsqueeze(0),num_classes=2,include_background=False,average='macro',input_format='index')[0]
+    iou = intersection_over_union(mask_bbox.unsqueeze(0),M_bbox.unsqueeze(0),aggregate=True)
+    true_class = classes[y] if classes else y
+    pred_class = classes[y_pred] if classes else y_pred
+    plt.suptitle(f'True: {true_class} | Pred: {pred_class}\nIoU: {iou:.4f} | DSC = {dsc:.2f}',y=0.98,weight='bold',fontsize=14)
+    plt.tight_layout()
 
 def plot_history(epochs,history):
     plt.subplot(1,3,1)
@@ -90,3 +153,24 @@ def plot_history(epochs,history):
     plt.ylabel('DSC')
     plt.suptitle('Training History Curve')
     plt.tight_layout()
+
+def plot_distribution(data:dict,metric:str='dsc'):
+    '''
+    Plots the distribution of selected metric.
+    Args:
+        data (dict): Data dictionary obtained from evaluate() function.
+        metric (str, default="dsc"): Selected metric.
+            Availabel metrics are: "dsc" and "iou".
+    Returns:
+        None
+    '''
+    if metric.lower() == 'dsc':
+        items = dice_score(data['mask'],data['M'],num_classes=2,include_background=False,average='macro',input_format='index')
+    elif metric.lower() == 'iou':
+        items = intersection_over_union(data['mask_bbox'],data['M_bbox'],aggregate=False).diagonal()
+    else:
+        raise NotImplementedError(f'Metric {metric} not yet implemented!')
+
+    sns.histplot(items)
+    plt.xlabel(metric)
+    plt.title(f'Median {metric.upper()}: {items.median():.4f}')

src/models/base_model.py
@@ -40,9 +40,9 @@ class BaseModel(nn.Module):
             nn.Conv2d(fmaps_count,128,kernel_size=3,stride=1,padding=1),
             nn.BatchNorm2d(128, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
             nn.ReLU(),
-            nn.Conv2d(128,128,kernel_size=3,stride=1,padding=1),
-            nn.BatchNorm2d(128, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
-            nn.ReLU(),
+            # nn.Conv2d(128,128,kernel_size=3,stride=1,padding=1),
+            # nn.BatchNorm2d(128, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+            # nn.ReLU(),
             nn.Conv2d(128,num_class,kernel_size=1,stride=1,padding=0)
         )
         self.gap = nn.AdaptiveAvgPool2d(output_size=(1,1))
@@ -64,18 +64,18 @@ class BaseModel(nn.Module):
         '''
         Helper function to create CAMs based on instance variable.
         Args:
-            threshold (float, default=0.7): Threshold to use for CAM to mask conversion. To be passed to _cam_to_mask method to convert CAMs into masks.
+            threshold (float || str, default=0.7): Threshold to use for CAM to mask conversion. To be passed to _cam_to_mask method to convert CAMs into masks. Pass "raw" to get raw cam instead of mask.
         Returns:
             cams (torch.Tensor): CAMs with a shape of Bx1xHxW.
         '''
         raise NotImplementedError('get_cam method has not been implemented.')
 
-    def _cam_to_mask(self,cams:torch.Tensor,threshold:float=0.7,img_size:tuple[int,int]=(224,224)) -> torch.Tensor:
+    def _process_cam(self,cams:torch.Tensor,threshold:float|str=0.7,img_size:tuple[int,int]=(224,224)) -> torch.Tensor:
         '''
         Helper function to convert raw CAM to prediction mask
         Args:
             cams (torch.Tensor): CAMs with Bx1xHxW.
-            threshold (float, default=0.7): Threshold to use for CAM to mask conversion.
+            threshold (float | str, default=0.7): Threshold to use for CAM to mask conversion. Use "raw" to keep raw CAM.
             img_size (tuple[int,int], default=(224,224)): The size of the original input image, used to scale the obtained masks back to original size.
         Returns:
             masks (torch.Tensor): Masks obtained from thresholding CAMs.
@@ -85,6 +85,7 @@ class BaseModel(nn.Module):
 
         norm_cam = ((cams-cams_min)/(cams_max-cams_min+1e-8))
         norm_cam = torch.nn.functional.interpolate(norm_cam,img_size,mode='bilinear')
-        
-        preds_masks = norm_cam >= threshold
-        return preds_masks
+
+        if threshold != 'raw':
+            norm_cam = norm_cam >= threshold
+        return norm_cam

src/models/cam.py
@@ -6,6 +6,7 @@ class CAM(BaseModel):
         super().__init__(num_class=num_class,backbone_model_type=backbone_model_type,ch_project=ch_project)
 
     def get_cam(self,threshold:float=0.7):
+        
         cams = self._fmaps_cls[torch.arange(self._fmaps_cls.shape[0]),self._preds,:,:].unsqueeze(1)
-        cams = self._cam_to_mask(cams,threshold=threshold)
+        cams = self._process_cam(cams,threshold=threshold)
         return cams

src/models/gradcam.py
@@ -39,5 +39,5 @@ class GradCAM(BaseModel):
         grad_weights = self.grads.mean(dim=(2,3)).unsqueeze(-1).unsqueeze(-1)
         cams = self._fmaps*grad_weights
         cams = torch.relu(torch.sum(cams,dim=1)).unsqueeze(1)
-        cams = self._cam_to_mask(cams,threshold=threshold)
+        cams = self._process_cam(cams,threshold=threshold)
         return cams